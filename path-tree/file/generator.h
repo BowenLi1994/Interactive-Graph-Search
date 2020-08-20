f8626118af32f9671a10d282e6c4104c2ec4817d
Bowen Li <bli@cs.fsu.edu>
2020-08-20 16:42:47 -0400

    parse path-tree

---
+++ path-tree/file/generator.h
@@ -0,0 +1,74 @@
+//
+//  path_tree_generator.h
+//  tree_interleave
+//
+//  Created by Bowen Li on 3/4/20.
+//  Copyright © 2020 Bowen Li. All rights reserved.
+//
+
+#pragma once
+
+
+namespace path_tree_file {
+
+using Label=label::StringLabel;
+using Node=node::Node<label::StringLabel>;
+using Supernode= supernode::Supernode;
+
+
+
+
+
+
+void generator(std::vector<Supernode *>& supernodes_collection,std::vector<std::string>& paths_string);
+void singel_generator(Supernode * sroot,std::string& path_string);
+std::string nodes_combine(Supernode* snode);
+
+
+
+void generator(std::vector<Supernode *>& supernodes_collection,std::vector<std::string>& paths_string){
+    
+    for(auto supernode: supernodes_collection){
+        
+        std::string path_string;
+        singel_generator(supernode,path_string);
+        paths_string.push_back(path_string);
+        //std::cout<<path_string<<std::endl;
+        
+
+        
+    }
+    
+    
+    
+}
+
+std::string nodes_combine(Supernode* snode){
+    std::string combined_node;
+    for(auto node: snode->getNodes())
+        combined_node=combined_node+std::to_string(node->id())+","+std::to_string(node->right_sibling())+"/";
+    
+    combined_node.erase(combined_node.end()-1);
+    return combined_node;
+}
+
+
+void singel_generator(Supernode * sroot, std::string& path_string){
+        
+    path_string+="{";
+    std::string combined_node=nodes_combine(sroot);
+    path_string+=combined_node;
+    for(auto schild: sroot->getChildren()){
+        singel_generator(schild, path_string);
+    }
+    path_string+="}";
+    
+}
+
+
+     
+    
+}
+
+
+
---
+++ path-tree/parser/path_tree_parser_impl.h
@@ -0,0 +1,212 @@
+/// \file parser/bracket_notation_parser_impl.h
+///
+/// \details
+/// Contains the implementation of the BracketNotationParser class.
+
+#pragma once
+
+/// This is currently a copy of the previous version but with the efficient
+/// tokanization.
+PathTreeParser::Supernode PathTreeParser::parse_single(
+    const std::string& tree_string) {
+
+
+  int id_counter=0;
+//  int depth_counter=0;
+
+  std::vector<std::string> tokens = get_tokens(tree_string);
+
+  // Tokenize the input string - get iterator over tokens.
+  auto tokens_begin = tokens.begin();
+  auto tokens_end = tokens.end();
+
+  // Deal with the root node separately.
+  ++tokens_begin; // Advance tokens to label.
+  std::string match_str = *tokens_begin;
+  if (match_str == kLeftBracket || match_str == kRightBracket) { // Root has an empty label.
+    match_str = "";
+    // Do not advance tokens - we're already at kLeftBracket or kRightBracket.
+  } else { // Non-empty label.
+    ++tokens_begin; // Advance tokens.
+  }
+    //std::cout<<match_str<<std::endl;
+    Supernode sroot;
+    sroot.set_id(id_counter);
+    std::vector<std::pair<int, int>> ids_pair=get_ids(match_str);
+    
+    for(auto pair: ids_pair){
+        
+        //std::cout<<"node: "<<pair.first<<" right: "<<pair.second<<std::endl;
+        Label node_label("path-tree");
+        Node* node=new Node(node_label);
+//        Node node(node_label);
+        node->set_id(pair.first);
+        node->set_right_sibling(pair.second);
+//        std::cout<<"node: "<<node.id()<<" right: "<<node.right_sibling()<<std::endl;
+        sroot.add_node(node);
+//          sroot.print_all_nodes();
+        
+    }
+  
+    node_stack.push_back(&sroot);
+    id_counter++;
+
+  //Iterate all remaining tokens.
+  while (tokens_begin != tokens_end) {
+
+    match_str = *tokens_begin;
+
+   if(match_str!=kLeftBracket && match_str!=kRightBracket)
+       break;
+
+
+    if (match_str == kLeftBracket) { // Enter node.
+      ++tokens_begin; // Advance tokens to label.
+      match_str = *tokens_begin;
+
+      if (match_str == kLeftBracket || match_str == kRightBracket) { // Node has an empty label.
+        match_str = "";
+        // Do not advance tokens - we're already at kLeftBracket or kRightBracket.
+      } else { // Non-empty label.
+        ++tokens_begin; // Advance tokens.
+      }
+
+      // Create new node.
+      Supernode*  snode=new Supernode;
+      snode->set_id(id_counter);
+      id_counter++;
+      std::vector<std::pair<int, int>> ids_pair=get_ids(match_str);
+
+        for(auto pair: ids_pair){
+
+            //std::cout<<"node: "<<pair.first<<" right: "<<pair.second<<std::endl;
+            Label node_label("path-tree");
+            Node* node=new Node(node_label);
+            
+            node->set_id(pair.first);
+            node->set_right_sibling(pair.second);
+            snode->add_node(node);
+        }
+
+        node_stack.back()->add_child(snode);
+        node_stack.push_back(snode);
+
+    }
+
+    if (match_str == kRightBracket) { // Exit node.
+      node_stack.pop_back();
+      ++tokens_begin; // Advance tokens.
+    }
+  }
+
+
+   
+    
+    return sroot;
+}
+
+void PathTreeParser::parse_collection(std::vector<Supernode*> &trees_collection, const std::string &file_path) {
+    
+  std::ifstream trees_file(file_path);
+  if (!trees_file) {
+    throw std::runtime_error("ERROR: Problem with opening the file '" + file_path + "' in BracketNotationParser::parse_collection_efficient.");
+  }
+  // Read the trees line by line, parse, and move into the container.
+  int tree_counter=0;
+  std::string tree_string;
+  while (std::getline(trees_file, tree_string)) {
+    std::cout<<"tree "<<tree_counter<<" "<<std::endl;
+    Supernode* node=new Supernode;
+    *node=parse_single(tree_string);
+//    Supernode node=parse_single(tree_string);
+//    node.print_heay_tree();
+////    Supernode* sroot=new Supernode;
+////    sroot=&node;
+//
+    trees_collection.push_back(node); // -> This invokes a move constructor (due to push_back(<rvalue>)).
+    tree_counter++;
+  }
+  trees_file.close();
+}
+
+
+// This is only a tokanizer that returns a vector with correct tokens.
+std::vector<std::string> PathTreeParser::get_tokens(
+    const std::string& tree_string) {
+  std::vector<std::string> tokens;
+
+  // Get pointer to the structure elements.
+  const char* s_elems = kStructureElements.c_str();
+
+  // Get pointer to the beginning of the input string.
+  const char* begin = tree_string.c_str();
+  // Pointer to the beginning of consecutive searches.
+  const char* next_begin = begin;
+  // Remember iter from previous iteration in old_iter - for label begin.
+  const char* old_iter = begin;
+  // iter is a pointer to consecutive occurences of either '{' or '}'.
+  for(const char* iter = strpbrk(next_begin, s_elems); iter != NULL; iter = strpbrk(next_begin, s_elems)) {
+    // Next iteration will start from the position right of iter.
+    next_begin = iter + 1;
+    // Check if the character just before the found position is an escape_char.
+    // Then, disregard the current position.
+    if (iter > begin && *(iter-1) == kEscapeChar) {
+      continue;
+    }
+    // If there is something between two consecutive brackets, it's potentially
+    // a label - record it.
+    if (iter > old_iter + 1) {
+      tokens.push_back(typename std::vector<std::string>::value_type(old_iter+1, iter)); // Calls the allocator of string.
+    }
+    // Record the found bracket.
+    tokens.push_back(typename std::vector<std::string>::value_type(iter, iter+1)); // Calls the allocator of string.
+    old_iter = iter;
+  }
+
+  return tokens;
+}
+
+bool PathTreeParser::validate_input(const std::string& tree_string) const {
+  int bracket_diff_counter = 0; // Counts difference between the numbers of left and right brackets.
+  int bracket_pair_counter = 0; // Counts number of bracket pairs - number of nodes assuming correct nesting.
+  // Loop over all characters.
+  for(auto it = tree_string.begin(); it != tree_string.end(); ++it) {
+    if (*it == kEscapeChar) { // Skip next character if kEscapeChar is found.
+      ++it;
+    } else if (*it == kLeftBracket[0]) { // Increase bracket_counter when kLeftBracket found.
+      bracket_diff_counter++;
+      bracket_pair_counter++;
+    } else if (*it == kRightBracket[0]) { // Decrease bracket_counter when kRightBracket found.
+      bracket_diff_counter--;
+    }
+  }
+  if (bracket_diff_counter != 0 || bracket_pair_counter == 0) {
+    return false;
+  }
+  return true;
+}
+
+
+ std::vector<std::pair<int, int>> PathTreeParser::get_ids(const std::string &node_string){
+     
+     std::vector<std::pair<int, int>> ids_set;
+     std::vector<std::string> nodes_set;
+     
+     functions::SplitString(node_string, nodes_set, "/");
+     
+     for(auto nodes: nodes_set){
+         std::vector<std::string> ids_nexsibling;
+         functions::SplitString(nodes, ids_nexsibling, ",");
+         ids_set.push_back(std::make_pair(std::stoi(ids_nexsibling[0]), std::stoi(ids_nexsibling[1])));
+     }
+     
+     
+     
+//     for(auto pair:ids_set){
+//         //std::cout<<"node id:"<<pair.first<<" right id: "<<pair.second<<std::endl;
+//     }
+     
+     
+     
+     return ids_set;
+ }
