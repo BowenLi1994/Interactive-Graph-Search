68ad3f767aab4ebb15b19756bfbd48d2b581d1d7
[BowenLi1994] <[bli@cs.fsu.edu]>
2020-09-03 02:12:05 -0400

    random

--- ranodm/random.h
+++ ranodm/random.h
@@ -1,29 +1,63 @@
 #pragma once
 #include "uniform.h"
+#include "normal.h"
+#include "zipf.h"
 
 
 namespace random_generator{
 
 
-void uniform();
+void uniform(int min, int max, int num_val,std::vector<int>& result);
+void normal(int mean,int stddev,int num_val,std::vector<int>& result);
+void zipf(int max, int alpha,int num_val,std::vector<int>& result);
 
-void uniform(){
-    
 
-	Dist_Genrator *dist = new Dist_Uniform(0, 100, 1);
+void uniform(int min, int max, int num_val,std::vector<int>& result){
 
-	for(int i = 0; i < 100; ++i){
-		fprintf(stderr, "%lu\n", dist->Next());
+	Dist_Genrator *dist = new Dist_Uniform(min, max, 1);
+
+	for(int i = 0; i < num_val; ++i){
+		result.push_back(dist->Next());
 	}
-
-	fprintf(stderr, "%s\n", dist->Name().c_str());
 
 	delete dist;
 
 }
 
 
+void normal(int mean,int stddev,int num_val,std::vector<int>& result){
 
+	Dist_Genrator *dist = new Dist_Normal(mean, stddev, time(NULL));
+
+	for(int i = 0; i < num_val; ++i){
+
+		int temp;
+		do{
+			temp=dist->Next();
+		}
+		while(temp<0 || temp > 2* mean-1);
+
+
+		
+		result.push_back(temp);
+	}
+	delete dist;
+}
+
+void zipf(int max, int alpha,int num_val,std::vector<int>& result){
+
+	Dist_Genrator *dist = new Dist_Zipf(max, alpha, false, 1);
+
+	for(int i = 0; i < num_val; ++i){
+		int temp;
+		do{
+			temp=dist->Next();
+		}while(temp<0);
+		result.push_back(temp);
+	}
+
+	delete dist;
+}
 
 
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,20 +4,27 @@
 #include <vector>
 
 
-
-
-
-
+#include "../ranodm/random.h"
 
 int main(int argc, const char * argv[]) {
+
+
+
     
 
 
-    
+
+    std::vector<int>result;
+    // random_generator::uniform(0,10,20,result);
+    random_generator::zipf(10,3,10,result);
+    for(auto i:result){
+      std::cout<<i<<std::endl;
+    }
     
 
   
     return 0;
 }
+
 
 
